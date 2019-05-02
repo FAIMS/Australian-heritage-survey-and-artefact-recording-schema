246dc9f5273f3500c763a7db5654f93652475f15
Brian Ballsun-Stanton <brian@fedarch.org>
2019-05-02 12:06:52 +1000

    preautomation vocab

---
+++ migrate_vocab.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+
+
+SPREADSHEET_ID = '1Qk9V33adnHjXs8HM8wIFL13UMKhKihYWTduIe7csays'
+
+def main():
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server()
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('sheets', 'v4', credentials=creds)
+
+    #
+    
+    sheet_metadata = service.spreadsheets().get(spreadsheetId=SPREADSHEET_ID).execute()
+    sheets = sheet_metadata.get('sheets', '')
+    for sheet in sheets:
+        title = sheet.get('properties').get('title')
+
+if __name__ == '__main__':
+    main()        
---
+++ postproc.sh
@@ -0,0 +1,149 @@
+#!/usr/bin/env bash
+
+cd module
+
+# string="
+#         <select1 ref=\"Entity_Types\">
+#           <label>\{Entity_Types}<\/label>
+#           <item>
+#             <label>Options not loaded<\/label>
+#             <value>Options not loaded<\/value>
+#           <\/item>
+#         <\/select1>"
+# replacement="
+#         <group ref=\"Colgroup_1\" faims_style=\"orientation\">
+#           <label\/>
+#           <group ref=\"Col_0\" faims_style=\"even\">
+#             <label\/>
+#             <select1 ref=\"Entity_Types\">
+#               <label>{Entity_Types}<\/label>
+#               <item>
+#                 <label>Options not loaded<\/label>
+#                 <value>Options not loaded<\/value>
+#               <\/item>
+#             <\/select1>
+#           <\/group>
+#           <group ref=\"Col_1\" faims_style=\"even\">
+#             <label\/>
+#             <select1 ref=\"Select_Author\">
+#               <label>{Select_Author}<\/label>
+#               <item>
+#                 <label>Options not loaded<\/label>
+#                 <value>Options not loaded<\/value>
+#               <\/item>
+#             <\/select1>
+#           <\/group>
+#         <\/group>"
+# perl -0777 -i.original -pe "s/$string/$replacement/igs" ui_schema.xml
+
+# string="
+#               <Entity_Types\/>"
+# replacement="
+#               <Colgroup_1>
+#                 <Col_0>
+#                   <Entity_Types\/>
+#                 <\/Col_0>
+#                 <Col_1>
+#                   <Select_Author\/>
+#                 <\/Col_1>
+#               <\/Colgroup_1>"
+# perl -0777 -i.original -pe "s/$string/$replacement/igs" ui_schema.xml
+
+# string="
+#         <trigger ref=\"Center_Me_1\">
+#           <label>\{Center_Me}<\/label>
+#         <\/trigger>
+#         <trigger ref=\"Create_Shape\">
+#           <label>\{Create_Shape}<\/label>
+#         <\/trigger>"
+# replacement="
+#         <group ref=\"Colgroup_1\" faims_style=\"orientation\">
+#           <label\/>
+#           <group ref=\"Col_1\" faims_style=\"even\">
+#             <label\/>
+#             <trigger ref=\"Center_Me_1\">
+#             <label>{Center_Me}<\/label>
+#             <\/trigger>
+#           <\/group>
+#           <group ref=\"Col_2\" faims_style=\"even\">
+#             <label\/>
+#             <trigger ref=\"Create_Shape\">
+#               <label>{Create_Shape}<\/label>
+#             <\/trigger>
+#           <\/group>
+#         <\/group>"
+# perl -0777 -i.original -pe "s/$string/$replacement/igs" ui_schema.xml
+
+# string="
+#               <Center_Me_1\/>
+#               <Create_Shape\/>"
+# replacement="
+#               <Colgroup_1>
+#                 <Col_1>
+#                   <Center_Me_1\/>
+#                 <\/Col_1>
+#                 <Col_2>
+#                   <Create_Shape\/>
+#                 <\/Col_2>
+#               <\/Colgroup_1>"
+# perl -0777 -i.original -pe "s/$string/$replacement/igs" ui_schema.xml
+
+# string="
+#               <Center_Me_1\/>
+#               <Create_Shape\/>"
+# replacement="
+#               <Colgroup_1>
+#                 <Col_1>
+#                   <Center_Me_1\/>
+#                 <\/Col_1>
+#                 <Col_2>
+#                   <Create_Shape\/>
+#                 <\/Col_2>
+#               <\/Colgroup_1>"
+# perl -0777 -i.original -pe "s/$string/$replacement/igs" ui_schema.xml
+
+
+# string="
+#         <trigger ref=\"Center_Me_1\">
+#           <label>\{Center_Me}<\/label>
+#         <\/trigger>
+#         <trigger ref=\"Create_Shape\">
+#           <label>\{Create_Shape}<\/label>
+#         <\/trigger>"
+# replacement="
+#         <group ref=\"Colgroup_1\" faims_style=\"orientation\">
+#           <label\/>
+#           <group ref=\"Col_1\" faims_style=\"even\">
+#             <label\/>
+#             <trigger ref=\"Center_Me_1\">
+#             <label>{Center_Me}<\/label>
+#             <\/trigger>
+#           <\/group>
+#           <group ref=\"Col_2\" faims_style=\"even\">
+#             <label\/>
+#             <trigger ref=\"Create_Shape\">
+#               <label>{Create_Shape}<\/label>
+#             <\/trigger>
+#           <\/group>
+#         <\/group>"
+# perl -0777 -i.original -pe "s/$string/$replacement/igs" ui_schema.xml
+
+# string="<input faims_attribute_name=\"Mound ID\" faims_attribute_type=\"measure\" ref=\"Mound_ID\" faims_style_class=\"required\">"
+
+# replacement="<input faims_attribute_name=\"Mound ID\" faims_attribute_type=\"measure\" ref=\"Mound_ID\" faims_style_class=\"moundID\">"
+# perl -0777 -i.original -pe "s/$string/$replacement/igs" ui_schema.xml
+
+# cat << EOF >> english.0.properties
+# Select_Author=Select Author
+# EOF
+
+# cat << EOF >> ui_styling.css
+# .moundID {
+#   color: #B2B2B2;
+# }
+# .moundID-label {
+#   color: red;
+# }
+# EOF
+
+# rm ui_schema.xml.original
