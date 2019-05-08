28984a8485cf2a8c97496aea4e8ffa04ee47ea4d
Brian Ballsun-Stanton <brian@fedarch.org>
2019-05-08 12:51:36 +1000

    custom logic

--- postproc.sh
+++ postproc.sh
@@ -11,29 +11,41 @@ string="
           <\/item>
         <\/select1>"
 replacement="
-        <group ref=\"Colgroup_1\" faims_style=\"orientation\">
-          <label\/>
-          <group ref=\"Col_0\" faims_style=\"even\">
-            <label\/>
-            <select1 ref=\"Entity_Types\">
-              <label>{Entity_Types}<\/label>
-              <item>
-                <label>Options not loaded<\/label>
-                <value>Options not loaded<\/value>
-              <\/item>
-            <\/select1>
-          <\/group>
-          <group ref=\"Col_1\" faims_style=\"even\">
-            <label\/>
-            <select1 ref=\"Select_Author\">
-              <label>{Select_Author}<\/label>
-              <item>
-                <label>Options not loaded<\/label>
-                <value>Options not loaded<\/value>
-              <\/item>
-            <\/select1>
-          <\/group>
-        <\/group>"
+         <group ref=\"Colgroup_1\" faims_style=\"orientation\">
+  <label\/>
+  <group ref=\"Col_0\" faims_style=\"even\">
+    <label\/>
+    <select1 ref=\"Entity_Types\">
+      <label>{Entity_Types}<\/label>
+      <item>
+        <label>Options not loaded<\/label>
+        <value>Options not loaded<\/value>
+      <\/item>
+    <\/select1>
+  <\/group>
+  <group ref=\"Col_1\" faims_style=\"even\">
+    <label\/>
+    <select1 ref=\"Select_Author\">
+      <label>{Select_Author}<\/label>
+      <item>
+        <label>Options not loaded<\/label>
+        <value>Options not loaded<\/value>
+      <\/item>
+    <\/select1>
+  <\/group>
+  <group ref=\"Col_2\" faims_style=\"even\">
+    <label\/>
+    <select1 ref=\"Select_Date\">
+      <label>{Select_Date}<\/label>
+      <item>
+        <label>Options not loaded<\/label>
+        <value>Options not loaded<\/value>
+      <\/item>
+    <\/select1>
+  <\/group>  
+  
+<\/group>
+"
 perl -0777 -i.original -pe "s/$string/$replacement/igs" ui_schema.xml
 
 string="
@@ -46,6 +58,10 @@ replacement="
                 <Col_1>
                   <Select_Author\/>
                 <\/Col_1>
+                <Col_2>
+                  <Select_Date\/>
+                <\/Col_2>
+                
               <\/Colgroup_1>"
 perl -0777 -i.original -pe "s/$string/$replacement/igs" ui_schema.xml
 
@@ -135,6 +151,8 @@ perl -0777 -i.original -pe "s/$string/$replacement/igs" ui_schema.xml
 
 cat << EOF >> english.0.properties
 Select_Author=Select Author
+Select_Date=Select Date
+Select_User=Select User
 EOF
 
 cat << EOF >> ui_styling.css
