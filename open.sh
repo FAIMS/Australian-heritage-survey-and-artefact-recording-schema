5fb369603a2a4b89b073eb0bc6e1d972245cc38b
Christian Nassif-Haynes <DoxasticFox@bitbucket.org>
2020-08-25 08:43:03 +1000

    Make open.sh work with restructured directories

--- open.sh
+++ open.sh
@@ -1,11 +1,18 @@
+#!/usr/bin/env bash
+
 # This script will execute the generate.sh script and then ./update_selenium.py
 
 set -euo pipefail
 
-for dir in $(find . -mindepth 2  -name "module.xml" | cut -d'/' -f2); do
-  echo $dir
-  cd $dir
-  generate.sh
-  ./upload_selenium.py --replace
-  cd ..
-done
+readonly script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+while IFS= read -r -d '' module
+do
+  dir=$(dirname "$module")
+  echo "$dir"
+  (
+    cd "$dir"
+    generate.sh
+    ./upload_selenium.py --replace
+  )
+done < <(find "$script_dir" -name module.xml -print0)
